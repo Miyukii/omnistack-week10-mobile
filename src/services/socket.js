38cbabc16d0b1d6362dc55dcf74e7c7504348b85
Wender Lucas <wenderlucas.souza@hotmail.com>
2020-01-18 17:08:11 -0300

    Subindo projeto

---
+++ src/services/socket.js
@@ -0,0 +1,35 @@
+import socketio from 'socket.io-client';
+
+const socket = socketio('http://192.168.100.39:3333', {
+    autoConnect: false,
+});
+
+function connect(latitude, longitude, techs) {
+    socket.io.opts.query= {
+        latitude,
+        longitude,
+        techs
+    }
+
+    socket.connect();
+
+    socket.on('message', text => {
+        console.log(text);
+    });
+}
+
+function disconnect() {
+    if(socket.connected){
+        socket.disconnect();
+    }
+}
+
+function subscribeToNewDevs(subscribeFunction){
+    socket.on('new-dev', subscribeFunction);
+};
+
+export {
+    connect,
+    disconnect,
+    subscribeToNewDevs
+};
